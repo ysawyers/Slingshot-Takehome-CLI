a57ef821f8b6f48ca4ab5ad7861c4cf90f142b80
Yonden Sawyers <yonden@Yondens-Air.fios-router.home>
2022-02-25 20:39:01 -0500

    index

---
+++ bin/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var axios = require('axios');
+
+describe('Insert', function () {
+  describe('#search door', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
